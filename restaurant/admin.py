b63e9d83ed3d5c51c80d85047995b6534ffc7f55
Sami Nafis <nafis@joinventureai.com>
2025-12-17 17:11:19 +0600

    feat: OrderItemInline for Order admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Category, MenuItem, Order
+from .models import Category, MenuItem, Order, OrderItem
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
-admin.site.register(Order)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
