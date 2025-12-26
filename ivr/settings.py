c3deeed1f8b683739ed2315e75d1b712b5bb9bbc
Sami Nafis <nafis@joinventureai.com>
2025-12-26 15:14:38 +0600

    add database based on dev environment

--- ivr/settings.py
+++ ivr/settings.py
@@ -110,7 +110,7 @@ PROD_DATABASES = {
     }
 }
 
-DATABASES = PROD_DATABASES
+DATABASES = LOCAL_DATABASES if DEBUG else PROD_DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
